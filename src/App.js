c3e13d5467959681f749975e94d872a917f071f9
Rupesh Thapa <thaparupesh767@gmail.com>
2024-08-12 12:54:49 +0545

    Fetching json file Practice

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route,Routes, Link } from "react-router-dom";
 import './App.css';
 import Home from './pages/Home';
-import AboutUs from "./pages/AboutUs";
+import AboutUs from './pages/AboutUs';
 import Contact from "./pages/Contact";
 
 function App(){
   return(
     <Router>
       <div className="App">
-        <header className="App-header">
-          <nav>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="AboutUs">About Us</Link></li>
-              <li><Link to="Contact">Contact</Link></li>
-            </ul>
-          </nav>
-          <Routes>
-            <Route path="/" element={Home}/>
-            <Route path="AboutUs" element={AboutUs}/>
-            <Route path="Contact" element={Contact}/>
-          </Routes>
+      <header className="App-header">
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="AboutUs">AboutUs</Link></li>
+            <li><Link to="Contact">Contact</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="AboutUs" element={<AboutUs/>}/>
+          <Route path="Contact" element={<Contact/>}/>
+        </Routes>
 
-        </header>
-
-      </div>
+      </header>
+    </div>
     </Router>
+    
   );
 }
 export default App;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Contact(){
+    const[contacts, setContacts] =useState([]);
+
+    useEffect(()=>{
+        fetch('/contact.json')
+        .then(response => response.json())
+        .then(data => setContacts(data));
+    }, []);
     return(
         <div>
-            <h2>Contact</h2>
+            <h2>Contact List</h2>
+            <ul>
+                {contacts.map(contact => (
+                    <li key={contact.id}>
+                        <h2>{contact.name}</h2>
+                        <p>{contact.age}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
